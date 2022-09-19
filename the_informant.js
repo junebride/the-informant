c759b60e24831bf9abb4f012ddf3da811e588e89
junebride <blazequ3en@gmail.com>
2022-09-19 17:14:26 +0800

    Add logger and nodemailer service

--- the_informant.js
+++ the_informant.js
@@ -1,14 +1,21 @@
 const fs = require("fs");
 const nodemailer = require("nodemailer");
+const logger = require("./the-logger/the_logger").init();
+
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 class Informant {
 	#transporter = null;
-	#mailOption = { to: "", subject: "" };
+	#mailOption = { to: "", subject: "", from:  };
 
 	constructor(mailOption = this.#mailOption) {
 		this.#mailOption.to = mailOption.to || this.#mailOption.to;
 		this.#mailOption.subject = mailOption.subject || this.#mailOption.subject;
+		this.#transporter = nodemailer.createTransport({
+			service: "gmail",
+			user: process.env.EMAIL,
+			pass: process.env.PASS
+		})
 	}
 
 	notify(message) {
@@ -16,11 +23,12 @@ class Informant {
 		mailOption["text"] = message;
 
 		this.#transporter.sendMail(mailOption, function (error, info) {
-			if (error) fs.appendFileSync(`${dir}/error.log`, `${error}\n`);
+			if (error) 
+			logger.error(notify, error)
 		});
 	}
 }
 
-module.exports.initLogger = function (options) {
+module.exports.init = function (options) {
 	return new Informant(options);
 };
